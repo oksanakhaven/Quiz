f3b57e839d0b9e612a52448714dd28f4c016fe42
Oksana Khaven <oksanakhaven@gmail.com>
2023-04-28 23:39:18 +0200

    third commit

--- script.js
+++ script.js
@@ -1,17 +1,70 @@
-const answersButtons = document.querySelectorAll('.answer');
-const correctAnswersButtons = document.querySelectorAll('.correct-answer');
-const checkingButton = document.querySelector('.checking');
-const showingPoints = document.querySelector('.points');
-const showingButton = document.querySelector('.showing');
-const retakingButton = document.querySelector('.retaking');
+const answersButtons = document.querySelectorAll(".answer");
+const correctAnswersButtons = document.querySelectorAll(".correct-answer");
+const checkingButton = document.querySelector(".checking");
+const showingPoints = document.querySelector(".points");
+const retakingButton = document.querySelector(".retaking");
 
-const array = [checkingButton, showingButton, retakingButton];
+let points = 0;
 
-function press(e) {
-    console.log ("Button " +e.target.name +" is pressed");
+// // ... - spread operator - add all elements to the list
+// const array = [...answersButtons, ...correctAnswersButtons, checkingButton, showingButton, retakingButton];
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     element.addEventListener("click", press, false);
+// }
+
+// function press(e) {
+//     //console.log ("Button " + e.target.textContent +" is pressed");
+// }
+
+//add style to CorrectAnswerButtons
+function showCorrect() {
+  for (let i = 0; i < correctAnswersButtons.length; i++) {
+    correctAnswersButtons[i].classList.add("show-correct");
+  }
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i]; 
-    element.addEventListener("click", press, false);
+
+//add yellow colour, when answers are selected
+for (let i = 0; i < answersButtons.length; i++) {
+  const el = answersButtons[i];
+  el.addEventListener("click", select, false);
 }
+function select() {
+    if(this.classList.contains('selected')){
+        this.classList.remove("selected");
+    }else{
+        this.classList.add("selected");
+    }
+}
+
+
+//add fuctionality to button Cheking and calculate points
+checkingButton.addEventListener("click", () => {
+  for (let i = 0; i < answersButtons.length; i++) {
+    if (
+      answersButtons[i].classList.contains("selected") &&
+      answersButtons[i].classList.contains("correct-answer")
+    ) {
+      points++;
+    }
+  }
+  showCorrect();
+  showingPoints.innerHTML = "Points: " + points;
+//make the button Cheking inactive, after showing the result
+  checkingButton.disabled = true;
+});
+
+
+//add fuctionality to button Retake
+retakingButton.addEventListener("click", () => {
+  for (let i = 0; i < answersButtons.length; i++) {
+    answersButtons[i].classList.remove("selected");
+    answersButtons[i].classList.remove("show-correct");
+  }
+  points = 0;
+  showingPoints.innerHTML = "Points: " + points;
+
+  checkingButton.disabled = false;
+});
